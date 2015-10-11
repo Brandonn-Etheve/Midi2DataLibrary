8bbb36f44d4b25680539483e960ade2aa99a4eb9
Brandonn <ethevebrandonn@cmail.nu>
2015-10-11 12:20:21 +0400

    add comments and getNoteArrays method

--- src/midiToData/MidiContainer.java
+++ src/midiToData/MidiContainer.java
@@ -14,16 +14,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package midiToData;
+package midi2Data;
 
+import exception.InstrumentNotFoundException;
 import java.util.Enumeration;
+import jm.music.data.Note;
+import jm.music.data.Part;
+import jm.music.data.Phrase;
 
 /**
- *
+ *This class try to store all music info into an object
  * @author brd
+ * 
  */
 public class MidiContainer {
-    private String title,timeSignature,keyQuality;
+   private String title,timeSignature,keyQuality;
    double beatsPerMinute,longuestRhytmValue,shortestRhytmValue;
    int keySignature;
    Enumeration  partList;
@@ -66,4 +71,74 @@ public class MidiContainer {
         this.partList=partList;
     }
     
+    public String getTitle()
+    {
+     return title;
+        
+    }
+    public String getTimeSignature()
+    {
+        return timeSignature;
+    }
+    
+    public double getBeatsPerMinute()
+    {
+        return beatsPerMinute;
+    }
+    public String getKeyQuality()
+    {
+        return keyQuality;
+    }
+    public int  getKeySignature()
+    {
+        return keySignature;
+    }
+    public double getLonguestRhytmValue()
+    {
+        return longuestRhytmValue;
+    }
+    public  Double getShortestRhytmValue()
+    {
+        return shortestRhytmValue;
+    }
+    public Enumeration  getPartList()
+    {
+        return partList;
+    }  
+    /**
+     * This method return an Arrays of all note for the instruments given in parrameters
+     * @param instrumentSearched
+     * @return
+     * @throws InstrumentNotFoundException 
+     */
+    public Note[] getNoteArrays(double instrumentSearched) throws InstrumentNotFoundException
+    {
+     
+           
+           Part part = null;
+           Phrase phrase;
+           Note[] noteArray = null;
+           Enumeration phraseEnum = null;
+           double instrument=-1;
+           while(partList.hasMoreElements()&&instrument!=instrumentSearched)
+           {
+               
+               part = (Part) partList.nextElement();
+               phraseEnum= part.getPhraseList().elements();
+               instrument=part.getInstrument();
+               //System.out.println(instrument);
+           }           
+            if(instrument!=instrumentSearched)
+            {
+                throw new InstrumentNotFoundException();
+            }
+           while(phraseEnum.hasMoreElements())
+           {
+              phrase =(Phrase) phraseEnum.nextElement();
+             noteArray= phrase.getNoteArray();
+             
+           } 
+           
+           return noteArray;
+    }
 }
